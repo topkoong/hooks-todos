c790fc6c3fd48bf2d5813e912da85384034b3317
topkoong <engineer_top@msn.com>
2019-06-07 03:21:34 -0400

    Removing Todos / "REMOVE_TODO" case

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -12,7 +12,7 @@ export default function TodoList() {
 					<li key={todo.id} className='flex items-center bg-orange-500 border-black border-dashed border-2 my-2 py-4'>
 						<span onDoubleClick={() => dispatch({type: 'TOGGLE_TODO', payload: todo})} className={`flex-1 ml-12 cursor-pointer ${todo.complete && 'line-through text-grey-900'}`}>{todo.text}</span>
 						<button><img src="https://icon.now.sh/edit/0050c5" alt="Edit Icon" className='h-6' /></button>
-						<button><img src="https://icon.now.sh/delete/8b0000" alt="Delete Icon" className='h-6' /></button>
+						<button onClick={() => dispatch({type: 'REMOVE_TODO', payload: todo})}><img src="https://icon.now.sh/delete/8b0000" alt="Delete Icon" className='h-6' /></button>
 					</li>
 				))}
 			</ul>
