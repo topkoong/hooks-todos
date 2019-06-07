d87cbe692c9256e81ec4fbc0faef6ac60f8a2b80
topkoong <engineer_top@msn.com>
2019-06-07 19:45:41 -0400

    Improving our App

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,12 @@ import uuidv4 from 'uuid/v4';
 export default function reducer(state, action) {
 	switch (action.type) {
 		case 'ADD_TODO':
+			if(!action.payload) {
+				return state;
+			}
+			if(state.todos.findIndex(t => t.text === action.payload) > -1) {
+				return state;
+			}
 			const newTodo = {
 				id: uuidv4(),
 				text: action.payload,
@@ -25,6 +31,12 @@ export default function reducer(state, action) {
 				todos: toggledTodo
 			}
 		case 'UPDATE_TODO':
+			if(!action.payload) {
+				return state;
+			}
+			if(state.todos.findIndex(t => t.text === action.payload) > -1) {
+				return state;
+			}
 			const updatedTodo = {...state.currentTodo, text: action.payload}
 			const updatedTodoIndex = state.todos.findIndex(t => t.id === state.currentTodo.id);
 			const updatedTodos = [
@@ -39,8 +51,10 @@ export default function reducer(state, action) {
 			}
 		case 'REMOVE_TODO':
 			const filteredTodos = state.todos.filter(t => t.id !== action.payload.id);
+			const isRemovedTodo = state.currentTodo.id === action.payload.id ? {} : state.currentTodo;
 			return {
 				...state,
+				currentTodo: isRemovedTodo,
 				todos: filteredTodos
 			}
 		default:
