d274b46b9e73916a0fa759bf9d8451d4c4d86941
topkoong <engineer_top@msn.com>
2019-06-07 00:14:34 -0400

    Avoiding Props Drilling with React Context and the useContext Hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import { UserContext } from './index';
 
-function App() {
+export default function App(props) {
+  const value = useContext(UserContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello, {value}
     </div>
   );
 }
 
-export default App;
