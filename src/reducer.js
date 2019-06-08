6c7b5671e1dc5a7ec24faa03d258aa09b6b36a40
topkoong <engineer_top@msn.com>
2019-06-07 21:14:18 -0400

    Performing Post Request to Add Todos

--- src/reducer.js
+++ src/reducer.js
@@ -8,18 +8,13 @@ export default function reducer(state, action) {
 				todos: action.payload
 			}
 		case 'ADD_TODO':
-			if(!action.payload) {
-				return state;
-			}
-			if(state.todos.findIndex(t => t.text === action.payload) > -1) {
-				return state;
-			}
-			const newTodo = {
-				id: uuidv4(),
-				text: action.payload,
-				complete: false
-			}
-			const addedTodos = [...state.todos, newTodo];
+			// if(!action.payload) {
+			// 	return state;
+			// }
+			// if(state.todos.findIndex(t => t.text === action.payload) > -1) {
+			// 	return state;
+			// }
+			const addedTodos = [...state.todos, action.payload];
 			return {
 				...state,
 				todos: addedTodos
